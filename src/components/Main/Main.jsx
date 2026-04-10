c072ab4b2421253152c8ec59b579bf17e6dba184
valihanov-web <valihanov057@gmail.com>
2026-04-10 12:43:40 +0500

    2555

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Main.css'
-
+import img1 from '../../assets/img1.png'
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+      <div className="main1">
+        <h1>Free trial session  <br /> with a trainer</h1>
+        <div className="btn">
+          <button>detailednpm</button>
+        </div>
+      </div>
+
+        <div className="main-img">
+          <img src={img1} alt="" />
+        </div>
+    </div>
   )
 }
 
