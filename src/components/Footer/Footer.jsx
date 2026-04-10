805dd1dcba09fb5bbbb5e3f868dcd56c21836ad4
Shaxriyor1227 <kiyotasakiayanakoji@gmail.com>
2026-04-10 12:56:58 +0500

    Second-commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import { FaInstagram } from "react-icons/fa6";
+import { CiFacebook } from "react-icons/ci";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+        <div className="gym">
+            <h1>GYM<mark>24</mark></h1>
+            <p>Build strength and confidence at our gym with state-of-the-art equipment, personalized training, and a motivating community</p>
+        </div>
+        <div className="address">
+            <h2>Address</h2>
+            <p>Republic of Belarus <br /> Minsk city <br /> Dzerzhinsky Avenue 15</p>
+        </div>
+        <div className="contact">
+            <h2>Contact</h2>
+            <p>+375 29 123-45-67 <br /> +375 29 987-65-43 <br /> info@gym24.by</p>
+            <div className="contact-icon">
+                <FaInstagram />
+                <CiFacebook />
+                <FaTwitter />
+                <IoLogoYoutube />
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './Reasons.css'
 
+import icon1 from "../../assets/icon1.png"
+import icon2 from "../../assets/icon2.png"
+import icon3 from "../../assets/icon3.png"
+import icon4 from "../../assets/icon4.png"
+
+const data = [
+  {
+    icon: icon1,
+    title: "15000 sq.m.",
+    text: "A spacious area for your business needs"
+  },
+  {
+    icon: icon2,
+    title: "4 fitness zones",
+    text: "From cardio to functional and cycle. Separate area for boxing and MMA"
+  },
+  {
+    icon: icon3,
+    title: "200+ equipment",
+    text: "No queues. Premium equipment from LifeStyle, Hammer Strength, TechnoGym"
+  },
+  {
+    icon: icon4,
+    title: "24/7 access",
+    text: "Open all day, including holidays and weekends"
+  }
+]
+
 const Reasons = () => {
   return (
-    <div>Reasons</div>
+    <div className="reasons">
+      <h1>Reasons to join</h1>
+
+      <div className="reasons-list">
+        {data.map((item, index) => (
+          <div className="reason-card" key={index}>
+            <img src={item.icon} alt="icon" />
+            <h2>{item.title}</h2>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
