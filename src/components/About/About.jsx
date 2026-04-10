3af0f7a2f1af0a7ab1ac20af3918f37de1a60c08
ibrohim-valixanov-web <ibrohimvalixanov778@gmail.com>
2026-04-10 12:28:15 +0500

    7778

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import './About.css'
+import "./About.css"
+import img1 from "../../assets/img90.png"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="container">
+        <div className="about">
+            <div className="us">
+                <h1>About us</h1>
+            </div>
+            <div className="station">
+                <p>Gym24 is a leading fitness center located near Grushevka metro station, offering over 1500 sq. m of space dedicated to top-of-the-line workout equipment from leading brands such as Hammer Strength, Life Fitness, and TechnoGym. </p>
+                <p className='with'>With affordable membership options, Gym24 is accessible to everyone who wants to reach their fitness goals, whether it's building strength, increasing endurance, or losing weight.</p>
+                <p>Certified trainers at Gym24 provide expert guidance and support to develop personalized workout plans tailored to each individual's needs and goals. </p>
+            </div>
+        </div>
+        <div className="img">
+            <img src={img1} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
